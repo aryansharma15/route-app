2fbbdd2112727d0c4058514802d3cf19bf4d7c6e
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-28 21:11:43 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/landing";
+import Blog from "./components/blog";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<header>
+				{/* navbar */}
+				<nav className="navbar">
+					<div className="land-nav-a">
+						<Link to="/">Home</Link>
+						<Link to="/blog">Blog</Link>
+						<Link to="#">Functions</Link>
+						<Link to="#">About</Link>
+					</div>
+
+					<div className="land-nav-b">
+						<a href="#">Contact</a>
+					</div>
+				</nav>
+			</header>
+
+			<main>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/blog" element={<Blog />} />
+				</Routes>
+			</main>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/components/functions.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Functions() {
+  return (
+    <div>
+      {/* navbar */}
+      <nav className="navbar">
+        <div className="land-nav-a">
+          <a href="#">Blog</a>
+          <a href="#">Functions</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+
+        <div className="land-nav-b">
+          <a href="#">Contact</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Functions
